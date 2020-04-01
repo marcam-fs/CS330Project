1b5a36b7edb2dbb0f46d8bdc930d3516e52bcb68
Maria Azam <mariauazam@gmail.com>
2020-03-31 23:49:09 -0600

    Adding function prototypes
    I have added 2 function prototypes (getCommand and checkForCommand), which are utilized by main to deal with receiving user commands.

--- main.cpp
+++ main.cpp
@@ -9,17 +9,52 @@
  Revision History: This file was created on Tuesday, March 31, 2020.
  */
 
-#include <marcamOS.cpp>
+//#include "marcamOS.cpp"
+#include <cstdlib>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool checkForCommand(string command)
+{
+    if (command.length() > 0)
+        return true;
+
+    return false;
+}
+
+int i = 0;
 
 int main()
 {
     //Welcome user to this OS
     printf("Welcome to marcamOS, a command line operating system!\n");
 
-    //Continously check for user-input commands
+    string command;
+    bool commandEntered = 0;
+
     do
-    {}
-    while ()
+    {
+        //Repeatedly print shell name
+       cout << "[marcamOS]%" << i << "  ";
+
+        //Receive user input
+        getline(cin, command);
+
+        //Check if this is a command
+        commandEntered = checkForCommand(command);
+
+        //If a command was entered, break (stop receiving input)
+        if (commandEntered)
+        {
+            i++;
+            break;
+        }
+    } while (command.length() == 0);
 
     return 0;
 }
